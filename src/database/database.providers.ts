c5a350492494546ef193f76925960140c0a56d88
balaturgija <matej.balat@gmail.com>
2024-01-24 12:07:30 +0100

    Initialize database module and sequelize

---
+++ src/database/database.providers.ts
@@ -0,0 +1,39 @@
+import { join } from 'path';
+import { Transaction } from 'sequelize';
+import { Sequelize } from 'sequelize-typescript';
+import { SequelizeTypescriptMigration } from 'sequelize-typescript-migration-lts';
+
+export const databaseProviders = [
+  {
+    provide: 'SEQUELIZE',
+    useFactory: async () => {
+      const sequelize = new Sequelize({
+        dialect: 'postgres',
+        host: process.env.DB_HOST,
+        port: Number(process.env.DB_PORT),
+        username: process.env.DB_USERNAME,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+        models: [],
+        define: {
+          underscored: true,
+          paranoid: true,
+        },
+        transactionType: Transaction.TYPES.DEFERRED,
+        timezone: Intl.DateTimeFormat().resolvedOptions().timeZone,
+      });
+      await SequelizeTypescriptMigration.makeMigration(sequelize, {
+        outDir: join(process.cwd(), '/src/database/migrations'),
+        migrationName: 'migration',
+        preview: false,
+        useSnakeCase: true,
+      });
+      try {
+        await sequelize.authenticate();
+        return sequelize;
+      } catch (error) {
+        throw new Error(error.name);
+      }
+    },
+  },
+];
