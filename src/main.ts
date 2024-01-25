6e2341c0ccae4cab05246777d9c21f4cc6426e08
balaturgija <matej.balat@gmail.com>
2024-01-25 16:15:27 +0100

    Add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,30 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { VersioningType } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.enableVersioning({ type: VersioningType.URI, defaultVersion: '1' });
+
+  const config = new DocumentBuilder()
+    .setTitle('RW performance API')
+    .setVersion('1');
+
+  const tags = ['pokemons'];
+  for (const tag of tags) {
+    config.addTag(tag);
+  }
+
+  const document = SwaggerModule.createDocument(app, config.build());
+
+  SwaggerModule.setup('docs', app, document, {
+    swaggerOptions: {
+      persistAuthorization: true,
+    },
+  });
+
+  await app.listen(process.env.API_PORT);
 }
 bootstrap();
