061ace37cad0d9bb2f9d59bb7014a5146b4a0c35
balaturgija <matej.balat@gmail.com>
2024-01-29 10:02:00 +0100

    Fix import

--- src/redis/redis.config.ts
+++ src/redis/redis.config.ts
@@ -1,13 +1,14 @@
 import { CacheModuleAsyncOptions } from '@nestjs/cache-manager';
-import * as redisStore from 'cache-manager-redis-store';
+import { create } from 'cache-manager-redis-store';
 
 export const redisConfig: CacheModuleAsyncOptions = {
   isGlobal: true,
   useFactory: async () => {
     const store = {
-      store: redisStore,
-      host: process.env.REDIS_HOST,
-      port: process.env.REDIS_PORT,
+      store: create({
+        host: process.env.REDIS_HOST,
+        port: Number(process.env.REDIS_PORT),
+      }),
       // ttl: process.env.REDIS_TTL,
     };
     try {
