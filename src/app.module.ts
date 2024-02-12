970bdf843dae3c161916d3fd0365108dc4e26dcf
balaturgija <matej.balat@gmail.com>
2024-02-12 12:54:55 +0100

    Initialize transactions module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { ScheduleModule } from '@nestjs/schedule';
 
 import { DatabaseModule } from './database/database.module';
+import { OrdersModule } from './orders/orders.module';
 import { RedisModule } from './redis/redis.module';
 import { StocksModule } from './stocks/stocks.module';
-import { OrdersModule } from './orders/orders.module';
+import { TransactionsModule } from './transactions/transactions.module';
 
 @Module({
   imports: [
@@ -12,10 +14,12 @@ import { OrdersModule } from './orders/orders.module';
       envFilePath: `.env`,
       isGlobal: true,
     }),
+    ScheduleModule.forRoot(),
     RedisModule,
     DatabaseModule,
     StocksModule,
     OrdersModule,
+    TransactionsModule,
   ],
 })
 export class AppModule {}
---
+++ src/transactions/entities/transaction.entity.ts
@@ -0,0 +1,72 @@
+import { DataTypes } from 'sequelize';
+import {
+  AllowNull,
+  BelongsTo,
+  Column,
+  Default,
+  ForeignKey,
+  HasMany,
+  IsDecimal,
+  IsInt,
+  IsUUID,
+  Model,
+  PrimaryKey,
+  Table,
+} from 'sequelize-typescript';
+import { OrderEntity } from 'src/orders/entities/order.entity';
+import { v4 } from 'uuid';
+
+@Table({
+  tableName: 'transactions',
+  underscored: true,
+  createdAt: true,
+  updatedAt: true,
+  deletedAt: true,
+})
+export class TransactionEntity extends Model<TransactionEntity> {
+  @PrimaryKey
+  @IsUUID(4)
+  @AllowNull(false)
+  @Default(() => v4)
+  @Column({
+    type: DataTypes.UUID,
+  })
+  id: string;
+
+  @AllowNull(false)
+  @IsInt
+  @Column({
+    type: DataTypes.INTEGER,
+  })
+  quantity: number;
+
+  @AllowNull(false)
+  @IsDecimal
+  @Column({
+    type: DataTypes.DECIMAL(12, 2),
+  })
+  price: number;
+
+  @ForeignKey(() => OrderEntity)
+  @AllowNull(false)
+  @IsUUID(4)
+  @Column({
+    type: DataTypes.UUID,
+  })
+  buyOrderId: string;
+
+  @ForeignKey(() => OrderEntity)
+  @AllowNull(false)
+  @IsUUID(4)
+  @Column({
+    type: DataTypes.UUID,
+  })
+  sellOrderId: string;
+
+  /** Assotiations */
+  @BelongsTo(() => OrderEntity, 'buyOrderId')
+  buyTransactions: OrderEntity[];
+
+  @BelongsTo(() => OrderEntity, 'sellOrderId')
+  sellTransactions: OrderEntity[];
+}
